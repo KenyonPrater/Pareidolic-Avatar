4bfbd356663abc769edcf3743392ef0785439993
Kenyon Prater <kenyonprater3.14@gmail.com>
2015-07-13 08:15:38 -0700

    Added a little bit of softness to lines

--- Pareidolic-Avatar/drawinghandler.py
+++ Pareidolic-Avatar/drawinghandler.py
@@ -77,7 +77,7 @@ class Drawing():
         for i in range(int(y-radius-1), int(y+radius+2)):
             for j in range(int(x-radius-1), int(x+radius+2)):
                 if 0<=i<self._height and  0<=j<self._width:
-                    weight = (1-((i-y)**2 + (j-x)**2)/radius**2)*4*speed/radius
-                    weight = min(weight, 1)
+                    weight = 256*(1-((i-y)**2 + (j-x)**2)/radius**2)*speed/radius
+                    weight = min(weight, 255)
                     if weight > 0:
-                        self._data[int(j)][int(i)].mixColor(r,g,b,weight*256)
+                        self._data[int(j)][int(i)].mixColor(r,g,b,weight)
